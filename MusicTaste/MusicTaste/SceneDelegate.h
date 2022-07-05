6f53e3fe57cc34291e281896b0463b0deba833c1
Aman Abraham <amanial@mit.edu>
2022-07-05 15:29:09 -0700

    Created Project Structure

---
+++ MusicTaste/MusicTaste/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  MusicTaste
+//
+//  Created by Aman Abraham on 7/5/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
