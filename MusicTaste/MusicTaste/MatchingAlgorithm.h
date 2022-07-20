acbc1c877a6456f31c1ada1c125d1896ccbfb20e
Aman Abraham <amanial@mit.edu>
2022-07-20 11:09:25 -0700

    Added MatchingAlgorithm File

---
+++ MusicTaste/MusicTaste/MatchingAlgorithm.h
@@ -0,0 +1,16 @@
+//
+//  MatchingAlgorithm.h
+//  MusicTaste
+//
+//  Created by Aman Abraham on 7/18/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MatchingAlgorithm : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MusicTaste/MusicTaste/MatchingAlgorithm.m
@@ -0,0 +1,12 @@
+//
+//  MatchingAlgorithm.m
+//  MusicTaste
+//
+//  Created by Aman Abraham on 7/18/22.
+//
+
+#import "MatchingAlgorithm.h"
+
+@implementation MatchingAlgorithm
+
+@end
